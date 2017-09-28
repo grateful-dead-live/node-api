82f9fa708364d9b1d0ad2da0bc98639c07ebec44
Florian Thalmann <florianthalmann@bluewin.ch>
2017-09-28 12:27:51 +0100

    added cors

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const fs = require('fs');
 const express = require('express');
 const N3 = require('n3');
 
-const app = express();
 const PORT = 8060;
 const TYPE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
 const LOCATION = "http://example.com/grateful_dead/vocabulary/location";
 const TIME = "http://purl.org/NET/c4dm/event.owl#time";
+
+const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const store = N3.Store();
 fs.readFile('rdf/event_main.ttl', 'utf8', (err, data) => {
