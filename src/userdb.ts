6939d133f71633ae6a286dc449a9482ade4b4432
muddymudskipper <thomas.wilmering@gmail.com>
2020-05-19 20:31:17 +0100

    comments fetch fix

--- src/userdb.ts
+++ src/userdb.ts
@@ -35,22 +35,11 @@ export async function checkBookmark(userid, route) {
 export async function getBookmarks(userid) {
     var x = await db.collection('testcollection').find( { 
         userId : userid, 
-    }).project({['bookmarks']:1}).toArray();
-    return x;
-}
-
-
-export async function _getComments(route) {
-    var s = route.split('/');
-    var x = await db.collection('testcollection').find( { 
-        name : 'comments',
-    }).project({ [s[2]]:1}).toArray();
-    this.getComments2(route);
+    }).project({'bookmarks':1}).toArray();
     return x;
 }
 
 export async function getComments(route) {
-    var s = route.split('/');
     var result = await db.collection('testcollection').aggregate([
         {$match: {'comments.route': route}},
         {$project: {
@@ -68,28 +57,16 @@ export async function getComments(route) {
 }
 
 export async function addComment(comment, route, userid) {
-    var s = route.split('/');
-    var c = JSON.parse(decodeURIComponent(comment));
-    console.log(c)  
-    db.collection('testcollection').updateOne( 
-        { name : 'comments'},
-        { $push: { [s[2]] : c } },
-        { upsert: true }
-    )
-    await this.addComment2(comment, route, userid)
-}
-
-export async function addComment2(comment, route, userid) {
     var c = JSON.parse(decodeURIComponent(comment));
     db.collection('testcollection').updateOne( 
-        { userId : userid },
+        { userId: userid },
         { $addToSet: { comments : { comment : c, route: route} } },
         { upsert: true }
     )
 }
 
 
-export async function checkComment(msgId, route) {
+export async function _checkComment(msgId, route) {
     var s = route.split('/');
     console.log('userdb: '+msgId)
     console.log(s[2]+'.msgId')
@@ -100,10 +77,20 @@ export async function checkComment(msgId, route) {
     return x+''
 }
 
-export async function getUserCommentRoutes(userid) {
-    var x = await db.collection('testcollection').find( 
-       { name: 'comments',
-        userId: "[userid]"}
-    ).toArray();
-    console.log(x);
+export async function checkComment(msgId, route) {
+    var result = await db.collection('testcollection').count({
+        'comments.comment.msgId': Number(msgId)
+    });
+    console.log(result);
+    return result+'';
 }
+
+// TODO: make work!
+export async function getUserCommentRoutes(userid) {   
+    var result = await db.collection('testcollection').find( 
+        { 'userId': userid }
+        ).project({'comments.route':1}).toArray();
+    console.log(result)
+}
+
+
