679d3b37cd43e4e84583c3e8ad463779ac95bc2f
muddymudskipper <thomas.wilmering@gmail.com>
2020-06-11 15:50:37 +0100

    get recording info from archive.org

---
+++ src/archive.ts
@@ -0,0 +1,15 @@
+
+
+import * as fetch from 'node-fetch';
+
+
+export async function getArchiveinfo(etree_id) {
+  console.log('GET');
+  const iaurl = "https://archive.org/advancedsearch.php?q=" + etree_id + "&fl%5B%5D=description&fl%5B%5D=notes&fl%5B%5D=source&fl%5B%5D=lineage&fl%5B%5D=subject&fl%5B%5D=date&output=json"
+  return fetch(iaurl)
+  .then(res => res.json())
+  .then(j => {
+    return j.response.docs[0];
+  })
+
+}
