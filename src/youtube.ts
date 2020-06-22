b98ca3b4a22f7116c24877a923e209dc3bf7f7e5
muddymudskipper <thomas.wilmering@gmail.com>
2020-06-22 10:33:47 +0100

    fetch and store youtube video list on mongodb

--- src/youtube.ts
+++ src/youtube.ts
@@ -7,43 +7,49 @@ export async function getYouTubeList(id, searchArray) {
     var timestamp = new Date;
     var y = await userDb.getYoutubeList(id);
     if (!y || timestamp.getTime() - y.timestamp > 604800000) {
-        //var res = await fetchYoutubeVideos(searchArray);
-        var res = [1,2,3];
+        var res = await fetchYoutubeVideos(searchArray);
         if (res) userDb.addYoutubelist(id, res, timestamp.getTime());
         else res = y;
     }
     else res = y;
-    console.log(res)
+    //console.log(res)
     return res  
 }
-
 
 async function fetchYoutubeVideos(searchArray): Promise<any>{
     searchArray = JSON.parse(searchArray);
     var searchString = '';
     searchArray.forEach(s => searchString += '"'+s+'"' + '+');
     searchString = searchString.slice(0, -1);
-    console.log(searchString);
+    console.log('youtube: ' + searchString);
     var r = await getPage(searchString);
-    console.log(r)
-    if (!r.items) result = null;
+    //console.log(r)
+    var result;
+    if (!r.items) result = undefined;
     else { 
-        var result = r.items;
+        var res;
+        res = r.items;
         r = await getPage(searchString, r.nextPageToken);
-        r.items.forEach(i => result.push(i));
-        console.log(result);
+        res.push(...r.items);
+        result = [];
+        res.forEach(v => {
+            result.push({
+                videoId: v.id.videoId,
+                title: v.snippet.title,
+                thumbnail: v.snippet.thumbnails.default.url
+            });
+        })
+        //console.log(result);
     }
     return result;
   }
-
-
 
 async function getPage(searchString, nextPageToken?): Promise<any> {
     const maxResults = 10;
     var pageToken;
     nextPageToken ? pageToken = nextPageToken : pageToken = '';
-    let url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&order=relevance&q=' + searchString + '&type=video&key=' 
-              + YOUTUBEAPIKEY + ' &pageToken=' + pageToken + '&maxresults=' + maxResults;
+    let url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&order=relevance&type=video&videoEmbeddable=true&q=' + searchString 
+              + '&key=' + YOUTUBEAPIKEY + ' &pageToken=' + pageToken + '&maxresults=' + maxResults;
     return fetch(url)
       .then(r => r.text())
       .then(t => JSON.parse(t))
